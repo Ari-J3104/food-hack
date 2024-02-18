2aa720f48bdf2ebfa2c7085cb8d81c5e3daf1cb5
ChinnyChilla <michaelzheng04@gmail.com>
2024-02-17 21:43:30 -0500

    push backend

--- backend/main.js
+++ backend/main.js
@@ -29,36 +29,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/api/uploadScreenshot', function (req, res) {
 	console.log('API works');
-	console.log(req.body);
-	// request.post('http://149.125.139.72:5000/predict', {
-	// 	json: {
-	// 		image: req.body.image,
-	// 	},
-	// }, (error, response, body) => {
-	// 	if (error) {
-	// 		console.error('Error during API call:', error);
-	// 		res.status(500).send('Error during API call');
-	// 	} else {
-	// 		console.log('API call successful');
-	// 	};
-	// });
-	request.post({url: "https://trackapi.nutritionix.com/v2/natural/nutrients",
-		
-			headers: {
-				'Content-Type': 'application/json',
-				'x-app-id': process.env.NUTRITIONIX_APP_ID,
-				'x-app-key': process.env.NUTRITIONIX_API_KEY,
-			},
-			body: JSON.stringify({	
-				query: "pizza",
-			}),
-		}, function(error, response, body) {
-			if (error) {
-				console.error('Error during API call:', error);
-				res.status(500).send('Error during API call');
-			}
-			res.status(200).send(body);		
-		})
+	request.post('http://149.125.139.72:5000/predict', {
+		json: {
+			image: req.body.image,
+		},
+	}, (error, response, body) => {
+		if (error) {
+			console.error('Error during API call:', error);
+			res.status(500).send('Error during API call');
+		} else {
+			console.log('API call successful');
+			console.log(body);
+			request.post({
+				url: "https://trackapi.nutritionix.com/v2/natural/nutrients",
+
+				headers: {
+					'Content-Type': 'application/json',
+					'x-app-id': process.env.NUTRITIONIX_APP_ID,
+					'x-app-key': process.env.NUTRITIONIX_API_KEY,
+				},
+				body: JSON.stringify({
+					query: "pizza",
+				}),
+			}, function (error, response, body) {
+				if (error) {
+					console.error('Error during API call:', error);
+					res.status(500).send('Error during API call');
+				}
+				res.status(200).send(body);
+			})
+		};
+	});
+	
 });
 
 
